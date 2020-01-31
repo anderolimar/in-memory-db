1a68e90b82f05ecafb7eab17b838d69092d7d520
Anderson Martins <anderolimar@gmail.com>
2020-01-31 07:27:43 -0300

    Update where-compare.js

--- src/where-compare.js
+++ src/where-compare.js
@@ -9,13 +9,18 @@ class WhereCompare {
     compare() {
         for(let key of Object.keys(this.where)) {
             let whereProp = this.where[key];
-            let refValue = this.ref[key];
-            for(let k of Object.keys(whereProp)){
+            let refValue = this.getValue([key], this.ref);
+            for(let k of Object.keys(whereProp)) {
                 let positive = Operators[k](refValue, whereProp[k]);
                 if(!positive) return false;
             }
         }
         return true;
+    }
+    
+    getValue(key, obj) {
+        const splitKeys = key.split('.')
+        return splitKeys.reduce((res,val) => res[val], obj)
     }
 }
 
